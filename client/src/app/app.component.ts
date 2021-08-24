c63af5d87ccbe8f73502cf6605c5b6f8e364c6fa
Matthew Esposito <matthew_esposito@hcl.com>
2021-08-24 17:00:32 -0400

    End of Section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit {
   title = 'The Datting App';
   users: any;
 
-  constructor(private accountServices: AccountService) {}
+  constructor(private accountServices: AccountService, private presence: PresenceService) {}
   
   ngOnInit() {
     this.setCurrentUser();
@@ -20,7 +21,10 @@ export class AppComponent implements OnInit {
 
   setCurrentUser(){
     const user: User = JSON.parse(localStorage.getItem('user'));
-    this.accountServices.setCurrentUser(user);
+    if (user) {
+      this.accountServices.setCurrentUser(user);
+      this.presence.createHUbConnection(user);
+    }
   }
 
 }
